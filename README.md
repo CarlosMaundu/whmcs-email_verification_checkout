38b787633f6fd76f56d676659be208bf533fcbc9
Carlos Maundu <carlmaundu@gmail.com>
2023-05-30 14:03:22 +0300

    Update files with my changes

--- README.md
+++ README.md
@@ -6,16 +6,21 @@ This code provides email verification functionality for the checkout process in 
 
 1. Download the latest release of the WHMCS email verification checkout code.
 
-2. Upload the file `email_verification_checkout.php` to the `includes/hooks/` directory in your WHMCS installation.
+2. Upload the file email_verification_checkout.php to the includes/hooks/ directory in your WHMCS installation.
 
-3. Open the `email_verification_checkout.php` file and set the configuration options according to your preferences:
-   - `PREVENTUNVERIFIEDORDERS`: Set to `true` to prevent unverified accounts from placing orders. Set to `false` to accept orders regardless of email verification status.
-   - `DEACTIVATEACCOUNTAFTERXDAYS`: Set the number of days to wait before deactivating unverified accounts. Set to `0` to disable this feature.
-   - `CLOSEACCOUNTAFTERXDAYS`: Set the number of days to wait before setting unverified accounts as closed. Set to `0` to disable this feature.
+3. Open the email_verification_checkout.php file and set the configuration options according to your preferences:
+
+# Constants for configuration
+# Prevent unverified accounts from placing orders (true to prevent, false to allow)
+define("VERIFY_EMAIL_ORDERS", true);
+# Number of days to wait before deactivating an unverified account (set to 0 to disable)
+define("ACCT_DEACT_DAYS", 0);
+# Number of days to wait before closing an unverified account (set to 0 to disable)
+define("ACCT_CLOSE_DAYS", 0);
 
 4. Save the file.
 
-5. Verify that your WHMCS installation has the necessary requirements, such as the WHMCS API and the Illuminate Database Capsule.
+5. Ensure that your WHMCS installation has the necessary requirements, such as the WHMCS API and the Illuminate Database Capsule.
 
 6. Run the daily cron job in WHMCS to handle account deactivation and closure according to the configured timeframes.
 
@@ -26,3 +31,13 @@ Once the code is installed and configured, it will enforce email verification fo
 ## License
 
 This code is released under the [MIT License](LICENSE).
+
+## Updates
+
+In the updated version, the script is using hooks to prevent unverified users from placing orders, deactivating unverified accounts after a specified number of days, and closing unverified accounts after a certain number of days. An additional hook for email verification during checkout is also included. Moreover, it includes additional functions for logging activity, sending admin notifications, fetching accounts for closing or deactivating, and checking if an email is verified.
+
+This version also leverages WHMCS's Illuminate Database Capsule for making database queries, providing a more expressive, fluent interface to creating and running database queries.
+
+
+
+
--- whmcs-email_verification_checkout.php
+++ whmcs-email_verification_checkout.php
@@ -1,72 +1,94 @@
 <?php
 
 if (!defined("WHMCS"))
-    die("Can't access the file directly!");
+    die("This file cannot be accessed directly");
 
 use WHMCS\View\Menu\Item as MenuItem;
 use Illuminate\Database\Capsule\Manager as Capsule;
 
-# Would you like to prevent unverified accounts from placing orders? Set it to false to accept orders
-define("PREVENTUNVERIFIEDORDERS", true);
-# How many days to wait before deactivating the unverified account, set 0 to deactivate this feature
-define("DEACTIVATEACCOUNTAFTERXDAYS", 0);
-# How many days to wait before setting the unverified account as closed, set 0 to disable this feature
-define("CLOSEACCOUNTAFTERXDAYS", 0);
+# Constants for configuration
+# Prevent unverified accounts from placing orders (true to prevent, false to allow)
+define("VERIFY_EMAIL_ORDERS", true);
+# Number of days to wait before deactivating an unverified account (set to 0 to disable)
+define("ACCT_DEACT_DAYS", 0);
+# Number of days to wait before closing an unverified account (set to 0 to disable)
+define("ACCT_CLOSE_DAYS", 0);
 
-# Orders will not be completed if the email is not verified.
+# Function to log activity
+function logActivity($description, $clientid = null) {
+    // Log activity in WHMCS
+    logActivity($description, $clientid);
+}
+
+# Function to send admin notifications
+function sendAdminNotification($subject, $message) {
+    // Send an admin notification in WHMCS
+    localAPI('SendAdminEmail', [
+        'customsubject' => $subject,
+        'custommessage' => $message
+    ], 'admin');
+}
+
+# Function to fetch accounts for closing or deactivating
+function fetchAccounts($days) {
+    $dateCreated = date("Y-m-d", strtotime("now - " . intval($days) . " days"));
+    return Capsule::table("tblclients")
+        ->where("datecreated", "=", $dateCreated)
+        ->where("email_verified", "=", 0)
+        ->get();
+}
+
+# Hook to prevent orders from unverified accounts
 add_hook("ShoppingCartValidateCheckout", 1, function($vars) {
-    if (PREVENTUNVERIFIEDORDERS === true) {
-        // Get the client data
+    if (VERIFY_EMAIL_ORDERS === true) {
         $client = Menu::context("client");
-        // Verify if the client is logged in and if it is found
         if (!is_null($client) && $client) {
-            // Check if the email is not verified
             if ($client->isEmailAddressVerified() == false) {
-                // Check if the cart contains products
                 if ($vars['cart']->getCount() > 0) {
-                    // Clear the cart to prevent order placement
                     $vars['cart']->clear();
                 }
-                return array("<b>Make sure you verify your email before proceeding with the order.</b>");
+                return array("<b>Before proceeding with your order, please verify your email.</b>");
             }
         }
     }
 });
 
-# Deactivate unverified account after x days
+# Hook to deactivate unverified accounts after x days
 add_hook("DailyCronJob", 1, function($vars) {
-    if (intval(DEACTIVATEACCOUNTAFTERXDAYS) !== 0) {
-        $dateCreated = date("Y-m-d", strtotime("now - " . intval(DEACTIVATEACCOUNTAFTERXDAYS) . " days"));
-        $getAccounts = Capsule::table("tblclients")->where("datecreated", "=", $dateCreated)->where("email_verified", "=", 0);
-        foreach ($getAccounts->get() as $account) {
+    if (intval(ACCT_DEACT_DAYS) !== 0) {
+        $accountsToDeactivate = fetchAccounts(ACCT_DEACT_DAYS);
+        foreach ($accountsToDeactivate as $account) {
             Capsule::table("tblclients")->where("id", $account->id)->update(array("status" => "Inactive"));
+            
+            // Log the action and send an admin notification
+            logActivity("Account ID: {$account->id} was deactivated due to email verification timeout.", $account->id);
+            sendAdminNotification("Account Deactivated", "Account ID: {$account->id} was deactivated due to email verification timeout.");
         }
     }
 });
 
-# Close unverified accounts after X days
+# Hook to close unverified accounts after X days
 add_hook("DailyCronJob", 1, function($vars) {
-    if (intval(CLOSEACCOUNTAFTERXDAYS) !== 0) {
-        $dateCreated = date("Y-m-d", strtotime("now - " . intval(CLOSEACCOUNTAFTERXDAYS) . " days"));
-        $getAccounts = Capsule::table("tblclients")->where("datecreated", "=", $dateCreated)->where("email_verified", "=", 0);
-        foreach ($getAccounts->get() as $account) {
+    if (intval(ACCT_CLOSE_DAYS) !== 0) {
+        $accountsToClose = fetchAccounts(ACCT_CLOSE_DAYS);
+        foreach ($accountsToClose as $account) {
             Capsule::table("tblclients")->where("id", $account->id)->update(array("status" => "Closed"));
+            
+            // Log the action and send an admin notification
+            logActivity("Account ID: {$account->id} was closed due to email verification timeout.", $account->id);
+            sendAdminNotification("Account Closed", "Account ID: {$account->id} was closed due to email verification timeout.");
         }
     }
 });
 
+# Hook for email verification during checkout
 add_hook('ClientAreaPage', 1, function($vars) {
     $currentPage = $vars['filename'];
 
-    // Check if the current page is the checkout page
     if ($currentPage == 'cart') {
-        if ($_POST['custtype'] == 'account') {
-            // Email verification is required for new accounts
-            if (!isEmailVerified($vars['clientsdetails']['email'])) {
-                redir("a=checkout&emailverification=required");
-            }
-        } else {
-            // Email verification is required for existing accounts
+        $custType = filter_input(INPUT_POST, 'custtype', FILTER_SANITIZE_STRING);
+
+        if ($custType == 'account' || $custType == 'existing') {
             if (!isEmailVerified($vars['clientsdetails']['email'])) {
                 redir("a=checkout&emailverification=required");
             }
@@ -74,8 +96,12 @@ add_hook('ClientAreaPage', 1, function($vars) {
     }
 });
 
+# Function to check if email is verified
 function isEmailVerified($email) {
-    $result = select_query('tblclients', 'id', array('email' => $email, 'email_verified' => '1'));
-    $data = mysql_fetch_array($result);
-    return $data['id'] ? true : false;
+    $account = Capsule::table('tblclients')
+        ->where('email', $email)
+        ->where('email_verified', '1')
+        ->first();
+
+    return $account ? true : false;
 }
